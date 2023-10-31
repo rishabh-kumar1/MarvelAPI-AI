c66a0b34fce9eaf68fd2a4039841beacff022760
rishabh-kumar1 <rishkum@umich.edu>
2023-10-31 15:53:32 -0400

    endpoint helper funcs + api call chain in jsx

--- app.py
+++ app.py
@@ -7,6 +7,20 @@ from textblob import TextBlob
 app = Flask(__name__)
 CORS(app)
 
+# Load SpaCy NER model
+nlp = spacy.load("en_core_web_sm")
+
+
+def named_entity_recognition(text):
+    doc = nlp(text)
+    entities = [ent.text for ent in doc.ents if ent.label_ == "PERSON"]
+    return entities
+
+
+def sentiment_analysis(text):
+    analysis = TextBlob(text)
+    sentiment = "positive" if analysis.sentiment.polarity > 0 else "negative" if analysis.sentiment.polarity < 0 else "neutral"
+    return sentiment
 
 @app.route("/classify", methods=["POST"])
 def computeClassifications():
--- marvel-character-grid/src/App.js
+++ marvel-character-grid/src/App.js
@@ -6,31 +6,58 @@ import './App.css';
 
 function App() {
  
+  const [characters, setCharacters] = useState([]);
+
   useEffect(() => {
     // Define your Marvel API public and private keys
     const publicKey = 'ed0d1ef9be26a5cd0799c4d22b473468';
     const privateKey = '4dd21eaa43edac6bd795410af035dc6bc697c770';
 
+
     // Generate a timestamp
     const timestamp = new Date().getTime();
+
 
     // Create an MD5 hash of timestamp, private key, and public key
     const hash = md5(timestamp + privateKey + publicKey);
 
-    //calling marvel endpoint
     axios
-    .get('http://gateway.marvel.com/v1/public/characters', {
-      params: {
-        ts: timestamp,
-        apikey: publicKey,
-        hash: hash
-      },
-    })
-    .then((response) => {
-      const characterData = response.data.data.results;
-    })
-  });
+      .get('http://gateway.marvel.com/v1/public/characters', {
+        params: {
+          ts: timestamp,
+          apikey: publicKey,
+          hash: hash,
+        },
+      })
+      .then((response) => {
+        const characterData = response.data.data.results;
+        // Clean up data and send it to Flask API
+        const cleanedCharacterData = characterData.map(character => {
+          return {
+            name: character.name,
+            description: character.description,
+            thumbnail: `${character.thumbnail.path}.${character.thumbnail.extension}`,
+            sentiment: ''
+          };
+        });
 
+        axios.post('http://localhost:5000/classify', cleanedCharacterData, {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+          .then((response) => {
+            console.log(response.data);
+            setCharacters(response.data);
+          })
+          .catch((error) => {
+            console.error('Error fetching data:', error);
+          });
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  });
 
 
   //grid ui should look something like this
