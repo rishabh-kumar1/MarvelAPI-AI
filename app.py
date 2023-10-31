104c8450d87308d86b3bdb1b4a9fc5dd8d810d4c
rishabh-kumar1 <rishkum@umich.edu>
2023-10-31 16:33:44 -0400

    ML enrichments + pagination capabilities + better documentation

--- app.py
+++ app.py
@@ -3,24 +3,23 @@ from flask_cors import CORS
 import spacy
 from textblob import TextBlob
 
-
 app = Flask(__name__)
-CORS(app)
+CORS(app) #Making sure we don't run into CORS restrictions when using multiple ports on our local!
 
-# Load SpaCy NER model
+# Load SpaCy NER model for proper noun recognition
 nlp = spacy.load("en_core_web_sm")
-
 
 def named_entity_recognition(text):
     doc = nlp(text)
     entities = [ent.text for ent in doc.ents if ent.label_ == "PERSON"]
     return entities
 
-
+#trying to determine sentiment associated with certain proper nouns included in descriptions
 def sentiment_analysis(text):
     analysis = TextBlob(text)
     sentiment = "positive" if analysis.sentiment.polarity > 0 else "negative" if analysis.sentiment.polarity < 0 else "neutral"
     return sentiment
+
 
 @app.route("/classify", methods=["POST"])
 def computeClassifications():
@@ -29,11 +28,12 @@ def computeClassifications():
         entities = named_entity_recognition(character['description'])
         sentiments = []
 
-
         for entity in entities:
             sentiment = sentiment_analysis(entity)
             sentiments.append(sentiment)
-
+        
+        namedEntitySentiment = named_entity_recognition(character['name'])
+        sentiments.append(namedEntitySentiment)
 
         # Count the occurrences of each sentiment
         sentiment_counts = {
@@ -42,11 +42,9 @@ def computeClassifications():
             'Neutral': sentiments.count('neutral')
         }
 
-
         # Get the sentiment with the highest count
         max_sentiment = max(sentiment_counts, key=sentiment_counts.get)
         character['sentiment'] = max_sentiment
-
 
     return jsonify(data)
 
--- marvel-character-grid/src/App.js
+++ marvel-character-grid/src/App.js
@@ -5,32 +5,39 @@ import './App.css';
 
 
 function App() {
- 
-  const [characters, setCharacters] = useState([]);
+  const [characters, setCharacters] = useState([]); //stateful character value with setter
+  const [page, setPage] = useState(1); // Initialize page number to 1
+
 
   useEffect(() => {
-    // Define your Marvel API public and private keys
+    // Define Marvel API public and private keys
     const publicKey = 'ed0d1ef9be26a5cd0799c4d22b473468';
     const privateKey = '4dd21eaa43edac6bd795410af035dc6bc697c770';
-
 
     // Generate a timestamp
     const timestamp = new Date().getTime();
 
-
-    // Create an MD5 hash of timestamp, private key, and public key
+    // Create an MD5 hash digest of timestamp, private key, and public key
     const hash = md5(timestamp + privateKey + publicKey);
 
+    // Calculate the offset based on the current page number
+    const offset = (page - 1) * 20;
+
+
+    // Make the API request with offset to allow us to implement user 
+    //driven pagination
     axios
       .get('http://gateway.marvel.com/v1/public/characters', {
         params: {
           ts: timestamp,
           apikey: publicKey,
           hash: hash,
+          offset: offset, // Include offset
         },
       })
       .then((response) => {
         const characterData = response.data.data.results;
+
         // Clean up data and send it to Flask API
         const cleanedCharacterData = characterData.map(character => {
           return {
@@ -41,14 +48,16 @@ function App() {
           };
         });
 
+        /* This url might change based on the local port that you end up hosting the flask endpoint at! 
+        In an actual dev environment we would most likely want to host that endpoint in a docker image on the cloud*/
         axios.post('http://localhost:5000/classify', cleanedCharacterData, {
           headers: {
             'Content-Type': 'application/json'
           }
         })
-          .then((response) => {
+          .then((response) => { 
             console.log(response.data);
-            setCharacters(response.data);
+            setCharacters(response.data); 
           })
           .catch((error) => {
             console.error('Error fetching data:', error);
@@ -57,10 +66,10 @@ function App() {
       .catch((error) => {
         console.error('Error fetching data:', error);
       });
-  });
-
-
-  //grid ui should look something like this
+  }, [page]); // Adding 'page' as a dependency to this hook!
+      //we need these api's to be called in order
+      //and catch any errors along the way
+      //thus this .then .catch chain is necessary
   return (
     <div className="App">
       <div className="grid-container">
@@ -74,9 +83,13 @@ function App() {
           </div>
         ))}
       </div>
+      <div className="pagination">
+        <button onClick={() => setPage(prevPage => Math.max(prevPage - 1, 1))}>Previous Page</button>
+        <span>Page {page}</span>
+        <button onClick={() => setPage(prevPage => prevPage + 1)}>Next Page</button>
+      </div>
     </div>
   );
 }
-
 
 export default App;
